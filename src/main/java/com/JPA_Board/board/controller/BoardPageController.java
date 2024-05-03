2ec6baac1090a85d7279b0d9182a6b3e82847d01
minsol <0206cho@gmail.com>
2024-05-03 21:02:12 +0900

    feat: 등록페이지 구현

--- src/main/java/com/JPA_Board/board/controller/BoardPageController.java
+++ src/main/java/com/JPA_Board/board/controller/BoardPageController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// 게시글 페이지 매핑
 @Controller
 @RequestMapping("/board")
 public class BoardPageController {
